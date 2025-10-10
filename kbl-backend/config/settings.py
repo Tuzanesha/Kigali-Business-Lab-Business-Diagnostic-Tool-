902dcfa0fbaaa98222af2457522ce9da2f77df18
Chloe-sys <karlychloee12@gmail.com>
2025-10-10 12:29:55 +0200

    fixed static files

--- kbl-backend/config/settings.py
+++ kbl-backend/config/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -185,7 +186,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Optional: additional dirs for project-level static assets
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# WhiteNoise settings
+WHITENOISE_USE_FINDERS = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
